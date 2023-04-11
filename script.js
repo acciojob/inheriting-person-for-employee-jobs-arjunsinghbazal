5c7a0d7b1e6f062507dd1cef52ac9c9274dfb9b2
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-11 15:54:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,6 +19,12 @@ Employee.prototype.constructor = Employee;
 Employee.prototype.jobGreet = function() {
   console.log("Hello, my name is " + this.name + ", I am " + this.age + " years old, and my job title is " + this.jobTitle + ".");
 }
+const person1 = new Person('Alice', 25);
+person1.greet(); // logs "Hello, my name is Alice, I am 25 years old."
+
+const employee1 = new Employee('Bob', 30, 'Software Engineer');
+employee1.greet(); // logs "Hello, my name is Bob, I am 30 years old."
+employee1.jobGreet(); // logs "Hello, my name is Bob, I am 30 years old, and my job title is Software Engineer."
 
 // Do not change code below this line
 window.Person = Person;
